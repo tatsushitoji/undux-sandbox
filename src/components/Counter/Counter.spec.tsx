385b8ba660a26e3c7db3e5c7e5abe6123f77bb1b
ttoji <tatsushi.toji@gmail.com>
2018-08-31 17:31:32 +0900

    WIP

--- src/components/Counter/Counter.spec.tsx
+++ src/components/Counter/Counter.spec.tsx
@@ -3,15 +3,10 @@ import { mount } from 'enzyme';
 
 import { Counter } from '../Counter';
 
-<<<<<<< HEAD
 const text = 'Counter';
 
 it('renders the heading', () => {
   const result = mount(<Counter text={text} />).find('.counter');
-=======
-it('renders the heading', () => {
-  const result = mount(<Counter />).find('.counter');
->>>>>>> WIP
   console.log(result.text());
   //   expect(result).toBeTruthy();
 });
--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,3 +1,5 @@
 import { configure } from 'enzyme';
-import * as EnzymeAdapter from 'enzyme-adapter-react-16';
-configure({ adapter: new EnzymeAdapter() });
+import * as EnzymeAdapter from 'enzyme-adapter-react-16/';
+
+const anyEnzymeAdapter: any = EnzymeAdapter;
+configure({ adapter: new anyEnzymeAdapter() });
